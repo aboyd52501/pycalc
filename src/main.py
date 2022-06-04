6b07ef9ac388bfa77db43a816a4dd7c6dabf936f
aboyd52501 <aboyd52501@gmail.com>
2022-06-04 18:57:14 -0500

    Started main file

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import ttk
+
+class Calculator:
+    def __init__(self):
+        self.total = 0
+        self.current = ""
+        self.op_pending = ''
+
+def press_one():
+
+
+root = Tk()
+root.title("Calculator 🧮")
+
+root.mainloop()
